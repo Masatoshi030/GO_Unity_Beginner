44305f26ee898712d32a1faa23afb33baa85b1a2
Masatoshi_Kitada <ditgames.inc@gmail.com>
2024-07-25 19:52:03 +0900

    [ADD]#3テキストの内容

--- Assets/CubeScript.cs
+++ Assets/CubeScript.cs
@@ -4,25 +4,27 @@ using UnityEngine;
 
 public class CubeScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public float MoveSpeed = 0.03f;
+
+    // 旗が押されたら
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
+    // ずっと
     void Update()
     {
+
+        //移動する処理
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            //ここに前回 No.1で使ったX軸に移動するプログラムを書こう！
-            transform.Translate(0.003f, 0.0f, 0.0f);
+            transform.Translate(MoveSpeed, 0.0f, 0.0f);
         }
-
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            //ここに前回 No.1で使ったX軸に移動するプログラムを書こう！
-            transform.Translate(-0.003f, 0.0f, 0.0f);
+            transform.Translate(-MoveSpeed, 0.0f, 0.0f);
         }
     }
 }
